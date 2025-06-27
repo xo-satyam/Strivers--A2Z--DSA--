be9c4d6a582c8738322853525e1e3c3a395f0c0a
xo-satyam <satyamsharmaprep@gmail.com>
2025-06-27 20:56:56 +0530

    Add Missing_number

---
+++ Array/Easy/Missing_number.cpp
@@ -0,0 +1,20 @@
+//PROBLEM STATEMENT- Given an array nums containing n distinct numbers in the range [0, n], 
+//                   return the only number in the range that is missing from the array.
+//PROBLEM-https://leetcode.com/problems/missing-number/description/
+//SOLUTION:-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        if(nums[0]==1)return 0;
+        int j=0;
+        for(int i=0;i<nums.size()-1;i++){
+            
+            if(nums[i]+1!=nums[i+1]) return nums[i]+1;
+            j++;
+        }
+        if(j==nums.size()-1) return nums[j]+1;
+        return 0;
+    }
+};
