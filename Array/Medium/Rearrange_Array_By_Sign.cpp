8bea0a2b429a34cfbae5afc80aa2c2660b1c099c
xo-satyam <satyamsharmaprep@gmail.com>
2025-06-29 21:59:46 +0530

    Add Rearrange_Array_By_Sign

---
+++ Array/Medium/Rearrange_Array_By_Sign.cpp
@@ -0,0 +1,21 @@
+//PROBLEM STATEMENT- You are given a 0-indexed integer array nums of even length consisting of an equal number of 
+//                   positive and negative integers.
+//PROBLEM-https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+//SOLUTION:-
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>ans(nums.size(),0);
+        int pos=0,neg=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos+=2;
+            }else{
+                ans[neg]=nums[i];
+                neg+=2;
+            }
+        }
+        return ans;
+    }
+};
