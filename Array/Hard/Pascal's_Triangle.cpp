65f9a2d0628181dad6981bf2e66f002e4893ca34
xo-satyam <satyamsharmaprep@gmail.com>
2025-07-02 22:47:53 +0530

    Add Pascal's_Triangle

---
+++ Array/Hard/Pascal's_Triangle.cpp
@@ -0,0 +1,25 @@
+//PROBLEM STATEMENT- Given an integer numRows, return the first numRows of Pascal's triangle.
+//PROBLEM-https://leetcode.com/problems/pascals-triangle/description/ 
+//SOLUTION:-
+class Solution {
+public:
+    vector<int> row(int numRows) {
+        long long ans=1;
+        vector<int>ansrow;
+        ansrow.push_back(1);
+        for(int col=1;col<numRows;col++){
+            ans *= (numRows-col);
+            ans /=  col;
+            ansrow.push_back(ans);
+        }
+        return ansrow;
+        
+    }
+    vector<vector<int>> generate(int numRows){
+        vector<vector<int>>ans;
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(row(i));
+        }
+        return ans;
+    }
+};
