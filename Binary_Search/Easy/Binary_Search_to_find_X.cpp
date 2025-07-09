9dfc32669dc2f83e00dd113090a8ff6138910e7e
xo-satyam <satyamsharmaprep@gmail.com>
2025-07-09 20:13:59 +0530

    Add Binary_Search_to_find_X

---
+++ Binary_Search/Easy/Binary_Search_to_find_X.cpp
@@ -0,0 +1,21 @@
+//PROBLEM STATEMENT-Binary Search to find X in sorted array
+//PROBLEM-https://leetcode.com/problems/binary-search/
+//SOLUTION:-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size(); 
+        int start = 0,end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                end = mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+};
